dc4127c4daad7709b014f6d51ddd3860f1b90fa4
Mariia Ivaskiv <mivaskisoft@gmail.com>
2025-09-29 16:00:01 +0300

    home work 4

---
+++ lesson4/home-work4/hw-objects.js
@@ -0,0 +1,47 @@
+
+//Створити файл objects.js, у якому зробити комплексний об’єкт,
+//що мав би мінімум 2 рівні ієрархії, масив та метод, який виводитиме значення
+
+const user = {};
+
+user.name = 'Mariia';
+user['company'] = 'SoftServe';
+user['job position'] = 'manual QC engineer';
+user.hobbies = ['reading', 'pub-quizzes', 'TRX'];
+user.address = {
+    country: 'Ukraine',
+    citizenship: true,
+    city: 'Ivano-Frankivsk',
+    zip: 760010
+};
+
+console.log(user);
+console.log(JSON.stringify(user));
+
+console.log('---------object methods--------');
+
+const userKeys = Object.keys(user);
+const userValues = Object.values(user);
+const userEntries = Object.entries(user);
+console.log(userKeys, userValues, userEntries);
+
+console.log('---------this--------');
+
+user.introduction = function () {
+    console.log('Hi, my name is');
+    console.log( (this.name));
+};
+
+user.Hobbies = function () {
+    console.log ( 'My hobbies are:');
+    console.log((this.hobbies.join(', ')));
+};
+
+user.Location = function () {
+    console.log( 'I live in' );
+    console.log(this.address.city);
+};
+
+user.introduction();
+user.Hobbies();
+user.Location();
